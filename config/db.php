60a4af2dd89e1ba364dfecb4bade14862390c663
Rice Pavel <rice.pavel@gmail.com>
2020-02-06 23:10:41 +0500

    others

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=wifi',
-    'username' => 'root',
-    'password' => '',
+    'username' => 'wifi',
+    'password' => 'hhlUI1puzbMAn1wP',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
